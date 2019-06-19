9e2b8cefe433687849729989178c260a510d2d31
vivek <vivek.bnb@gmail.com>
2019-06-19 23:33:11 +0530

    working on enc

--- src/main/java/com/bnb/gj/general/license/ScratchSpace.java
+++ src/main/java/com/bnb/gj/general/license/ScratchSpace.java
@@ -2,7 +2,10 @@ package com.bnb.gj.general.license;
 
 import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import com.google.common.hash.HashCode;
@@ -10,44 +13,57 @@ import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
 public class ScratchSpace {
-
-	public static void main(String[] args) {
-		 String userName = "Max Headroom";
-	        String productKey = "ABCD";
-	        String versionNumber = "4";
-	        
-	        LocalDate today = LocalDate.now();
-	        String pattern = "MM-dd-yyyy";
-	        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-	        
-	        today.plusYears(1);
-	        
-	        String date = today.getMonthValue() +"-"+ today.getDayOfMonth() + "-" + today.getYear();
-	        System.out.println("date" + date);
-
-	        final String licenseKey = createLicenseKey(userName, productKey, versionNumber , date);
-	        System.out.println("licenseKey = " + licenseKey);
+	
+	final static public DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy MM dd HH:mm");
+	final static public DateTimeFormatter DATE_STD_FORMATTER = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+	
+	public static String getDateStringFromDate(Date date , DateTimeFormatter dateTimeFormatter) {
+		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime().format(dateTimeFormatter);
 
 	}
 
-	
-	public static String createLicenseKey(String userName, String productKey, String versionNumber, String date) {
-        final String s = userName + "|" + productKey + "|" + versionNumber + "|" +date ;
-        System.out.println(s);
-        final HashFunction hashFunction = Hashing.sha1();
-        final HashCode hashCode = hashFunction.hashString(s, Charset.defaultCharset());
-        final String upper = hashCode.toString().toUpperCase();
-        return group(upper);
-    }
 
-    private static String group(String s) {
-        String result = "";
-        for (int i=0; i < s.length(); i++) {
-            if (i%6==0 && i > 0) {
-                result += '-';
-            }
-            result += s.charAt(i);
-        }
-        return result;
-    }
+	public static void main(String[] args) {
+		String userName = "2654e905-f93b-4bc1-aa53-62e50386c275";
+		String productKey = "wiki";
+		String versionNumber = "1.2";
+
+		LocalDate today = LocalDate.now();
+		String pattern = "MM-dd-yyyy";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+		today.plusYears(1);
+
+		String date = today.getMonthValue() + "-" + today.getDayOfMonth() + "-" + today.getYear();
+		System.out.println("date" + date);
+		
+		var datestr = getDateStringFromDate(new Date(), DATE_STD_FORMATTER);
+		System.out.println("datestr"+datestr);
+		
+		date = "07-03-2019";
+
+		final String licenseKey = createLicenseKey(userName, productKey, versionNumber, date);
+		System.out.println("licenseKey = " + licenseKey);
+
+	}
+
+	public static String createLicenseKey(String userName, String productKey, String versionNumber, String date) {
+		final String s = userName + "|" + productKey + "|" + versionNumber + "|" + date;
+		System.out.println(s);
+		final HashFunction hashFunction = Hashing.sha1();
+		final HashCode hashCode = hashFunction.hashString(s, Charset.defaultCharset());
+		final String upper = hashCode.toString().toUpperCase();
+		return group(upper);
+	}
+
+	private static String group(String s) {
+		String result = "";
+		for (int i = 0; i < s.length(); i++) {
+			if (i % 6 == 0 && i > 0) {
+				result += '-';
+			}
+			result += s.charAt(i);
+		}
+		return result;
+	}
 }
