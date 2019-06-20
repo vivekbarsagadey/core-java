8047c71b683935851a97b9f8d31a5bb737180cf8
vivek <vivek.bnb@gmail.com>
2019-06-20 20:28:59 +0530

    working on encryption

---
+++ src/main/java/com/bnb/gj/general/encryption/JasyptEncryption.java
@@ -0,0 +1,22 @@
+package com.bnb.gj.general.encryption;
+
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
+public class JasyptEncryption {
+
+	public static void main(String[] args) {
+		String userPassword = "root";
+		String inputPassword = "root";
+		StrongPasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
+		String encryptedPassword = passwordEncryptor.encryptPassword(userPassword);
+		System.out.println(encryptedPassword);
+		
+		if (passwordEncryptor.checkPassword(inputPassword, encryptedPassword)) {
+			System.out.println("Password matched");
+		}else {
+			System.out.println("Password Not matched");
+		}
+
+	}
+
+}
